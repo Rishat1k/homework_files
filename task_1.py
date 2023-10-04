b497239e5f28a3e9173aa8f5345c08ac4a12760b
Rishat1k <rishat190299@yandex.ru>
2023-10-04 15:38:08 +0300

    corrected

--- task_1.py
+++ task_1.py
@@ -1,22 +1,25 @@
-with open('recipes.txt', 'r', encoding='utf-8') as f:
-    lst_of_dishes = []
-    lst = []
-    for row in f.readlines():
-        if row.strip() != '':
-            lst.append(row.strip())
-        else:
-            lst_of_dishes.append(lst)
-            lst = []
-            continue
-    lst_of_dishes.append(lst)
+def create_cook_book(recipes):
+    with open(recipes, 'r', encoding='utf-8') as f:
+        lst_of_dishes = []
+        lst = []
+        for row in f.readlines():
+            if row.strip() != '':
+                lst.append(row.strip())
+            else:
+                lst_of_dishes.append(lst)
+                lst = []
+                continue
+        lst_of_dishes.append(lst)
+    cook_book = {}
+    for dish in lst_of_dishes:
+        lst = []
+        for i in range(2, len(dish)):
+            ingredient_name, quantity, measure = dish[i].split(' | ')
+            lst.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
+        cook_book[dish[0]] = lst
+    return cook_book
 
-cook_book = {}
-for dish in lst_of_dishes:
-    lst = []
-    for i in range(2, len(dish)):
-        ingredient_name, quantity, measure = dish[i].split(' | ')
-        lst.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
-    cook_book[dish[0]] = lst
-print(cook_book)
+print(create_cook_book('recipes.txt'))
+
 
 
--- task_2.py
+++ task_2.py
@@ -1,24 +1,26 @@
-with open('recipes.txt', 'r', encoding='utf-8') as f:
-    lst_of_dishes = []
-    lst = []
-    for row in f.readlines():
-        if row.strip() != '':
-            lst.append(row.strip())
-        else:
-            lst_of_dishes.append(lst)
-            lst = []
-            continue
-    lst_of_dishes.append(lst)
-
-cook_book = {}
-for dish in lst_of_dishes:
-    lst = []
-    for i in range(2, len(dish)):
-        ingredient_name, quantity, measure = dish[i].split(' | ')
-        lst.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
-    cook_book[dish[0]] = lst
+def create_cook_book(recipes):
+    with open(recipes, 'r', encoding='utf-8') as f:
+        lst_of_dishes = []
+        lst = []
+        for row in f.readlines():
+            if row.strip() != '':
+                lst.append(row.strip())
+            else:
+                lst_of_dishes.append(lst)
+                lst = []
+                continue
+        lst_of_dishes.append(lst)
+    cook_book = {}
+    for dish in lst_of_dishes:
+        lst = []
+        for i in range(2, len(dish)):
+            ingredient_name, quantity, measure = dish[i].split(' | ')
+            lst.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
+        cook_book[dish[0]] = lst
+    return cook_book
 
 def get_shop_list_by_dishes(dishes, person_count):
+    cook_book = create_cook_book('recipes.txt')
     shop_dict = {}
     for dish in dishes:
         for lst in cook_book[dish]:
@@ -28,5 +30,5 @@ def get_shop_list_by_dishes(dishes, person_count):
                 shop_dict[lst['ingredient_name']] = {'measure': lst['measure'], 'quantity': int(lst['quantity'])*person_count}
     return shop_dict
 
-print(get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2))
+print(get_shop_list_by_dishes(['Омлет', 'Омлет'], 2))
 
